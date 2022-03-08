0dd8cf8d2e1b5ce28e57fc74bb5dd89856b41b7b
Richards0n <richardson.douglas.31@gmail.com>
2022-03-07 22:18:14 -0300

    Adding esx_communityservice

---
+++ ESX/esx_communityservice/config.lua
@@ -0,0 +1,46 @@
+Config = {}
+
+Config.Locale = 'br'
+Config.ServiceExtensionOnEscape = 8
+Config.ServiceLocation = {x =  -2075.13, y = 2845.79, z = 32.81}
+Config.ReleaseLocation = {x = 428.52, y = -981.89, z = 30.71}
+
+
+Config.ServiceLocations = {
+	{ type = "cleaning", coords = vector3(-2075.58, 2843.4, 32.81) },
+	{ type = "cleaning", coords = vector3(-2069.9, 2851.03, 32.81) },
+	{ type = "cleaning", coords = vector3(-2064.09, 2845.21, 32.81) },
+	{ type = "cleaning", coords = vector3(-2057.64, 2836.0, 32.81) },
+	{ type = "cleaning", coords = vector3(-2065.73, 2859.02, 32.81) },
+	{ type = "cleaning", coords = vector3(-2081.57, 2845.21, 32.81) },
+	{ type = "cleaning", coords = vector3(-2075.64, 2863.21, 32.81) },
+	{ type = "cleaning", coords = vector3(-2098.08, -1000.55, 29.35) },
+	{ type = "gardening", coords = vector3(-2053.38, 2850.87, 32.81) },
+	{ type = "gardening", coords = vector3(-2047.42, 2863.5, 32.81)},
+	{ type = "gardening", coords = vector3(-2052.67, 2874.24, 32.81) },
+	{ type = "gardening", coords = vector3(-2063.04, 2878.6, 32.81) },
+	{ type = "gardening", coords = vector3(-2080.39, 2883.22, 32.81) }
+}
+
+Config.Uniforms = {
+	prison_wear = {
+		male = {
+			['tshirt_1'] = 15,  ['tshirt_2'] = 0,
+			['torso_1']  = 146, ['torso_2']  = 0,
+			['decals_1'] = 0,   ['decals_2'] = 0,
+			['arms']     = 119, ['pants_1']  = 3,
+			['pants_2']  = 7,   ['shoes_1']  = 12,
+			['shoes_2']  = 12,  ['chain_1']  = 0,
+			['chain_2']  = 0
+		},
+		female = {
+			['tshirt_1'] = 3,   ['tshirt_2'] = 0,
+			['torso_1']  = 38,  ['torso_2']  = 3,
+			['decals_1'] = 0,   ['decals_2'] = 0,
+			['arms']     = 120,  ['pants_1'] = 3,
+			['pants_2']  = 15,  ['shoes_1']  = 66,
+			['shoes_2']  = 5,   ['chain_1']  = 0,
+			['chain_2']  = 0
+		}
+	}
+}
---
+++ ESX/esx_communityservice/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+
+description 'ESX Community Service'
+
+version '1.1.1'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/br.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+dependency 'es_extended'
