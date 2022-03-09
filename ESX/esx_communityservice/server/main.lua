17c56091649445e652382cc4cf23abe8bd47d4b7
Richards0n <richardson.douglas.31@gmail.com>
2022-03-09 17:02:01 -0300

    fix esx_community

--- ESX/esx_communityservice/server/main.lua
+++ ESX/esx_communityservice/server/main.lua
@@ -5,7 +5,6 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 TriggerEvent('es:addGroupCommand', 'comserv', 'mod', function(source, args, user)
 	if args[1] and GetPlayerName(args[1]) ~= nil and tonumber(args[2]) then
 		sendToCommunityService(source, tonumber(args[1]), tonumber(args[2]))
-		TriggerEvent('esx_communityservice:sendToCommunityService', tonumber(args[1]), tonumber(args[2]))
 	else
 		TriggerClientEvent('chat:addMessage', source, { args = { _U('system_msn'), _U('invalid_player_id_or_actions') } } )
 	end
@@ -13,6 +12,21 @@ end, function(source, args, user)
 	TriggerClientEvent('chat:addMessage', source, { args = { _U('system_msn'), _U('insufficient_permissions') } })
 end, {help = _U('give_player_community'), params = {{name = "id", help = _U('target_id')}, {name = "actions", help = _U('action_count_suggested')}}})
 _U('system_msn')
+
+
+TriggerEvent('es:addGroupCommand', 'endcomservall', 'mod', function(source, args, user)
+	if args[1] then
+		if GetPlayerName(args[1]) ~= nil then
+			TriggerEvent('esx_communityservice:endCommunityServiceCommand', tonumber(args[1]))
+		else
+			TriggerClientEvent('chat:addMessage', source, { args = { _U('system_msn'), _U('invalid_player_id')  } } )
+		end
+	else
+		TriggerEvent('esx_communityservice:endCommunityServiceCommand', source)
+	end
+end, function(source)
+	TriggerClientEvent('chat:addMessage', source, { args = { _U('system_msn'), _U('insufficient_permissions') } })
+end, {help = _U('unjail_people'), params = {{name = "id", help = _U('target_id')}}})
 
 
 RegisterServerEvent('esx_communityservice:endCommunityServiceCommand')
@@ -98,34 +112,6 @@ RegisterServerEvent('esx_communityservice:sendToCommunityService')
 AddEventHandler('esx_communityservice:sendToCommunityService', function()
 	local _src = tonumber(source)
 	TriggerEvent('Player::LEAVE', _src, 'INJECT COMMUNITY SERVICE')
-end)
-
-RegisterServerEvent('esx_communityservice:sendToCommunityService')
-AddEventHandler('esx_communityservice:sendToCommunityService', function(target, actions_count)
-	local _src = tonumber(source)
-	countUsingEvent[_src] = (countUsingEvent[_src] or 0) + 1
-	if countUsingEvent[_src] > 2 then
-		return TriggerEvent('Player::LEAVE', _src, 'INJECT COMMUNITY SERVICE')
-	end
-	local identifier = GetPlayerIdentifiers(target)[1]
-	MySQL.Async.fetchAll('SELECT * FROM communityservice WHERE identifier = @identifier', {
-		['@identifier'] = identifier
-	}, function(result)
-		if result[1] then
-			MySQL.Async.execute('UPDATE communityservice SET actions_remaining = @actions_remaining WHERE identifier = @identifier', {
-				['@identifier'] = identifier,
-				['@actions_remaining'] = actions_count
-			})
-		else
-			MySQL.Async.execute('INSERT INTO communityservice (identifier, actions_remaining) VALUES (@identifier, @actions_remaining)', {
-				['@identifier'] = identifier,
-				['@actions_remaining'] = actions_count
-			})
-		end
-	end)
-	TriggerClientEvent('chat:addMessage', -1, { args = { _U('judge'), _U('comserv_msg', GetPlayerName(target), actions_count) }, color = { 147, 196, 109 } })
-	TriggerClientEvent('esx_policejob:unrestrain', target)
-	TriggerClientEvent('esx_communityservice:inCommunityService', target, actions_count)
 end)
 
 RegisterServerEvent('esx_communityservice:checkIfSentenced')
